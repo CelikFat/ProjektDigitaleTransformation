cf4163f6f703272bab36e166dc1b269c9fc0a8d8
speier-v <127931979+speier-v@users.noreply.github.com>
2023-11-26 15:50:11 +0100

    modify sponsor list and make the items align

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,48 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart' as google_fonts;
+import 'package:studi_cafe/home_page/sponsors_list_file.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(  
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xFFD98E44),
-              borderRadius: BorderRadius.circular(10.0), // Optional: Add border radius for rounded corners
-              border: Border.all(
-                color: Colors.white, // White border color
-                width: 8.0, // Adjust the width of the border
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(  
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFD98E44),
+                borderRadius: BorderRadius.circular(10.0), // Optional: Add border radius for rounded corners
+                border: Border.all(
+                  color: Colors.white, // White border color
+                  width: 8.0, // Adjust the width of the border
+                ),
               ),
-            ),
-            
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.4,
-            margin: const EdgeInsets.fromLTRB(0, 50, 0, 50),
-            child: Center(
-              child: Text (
-                "Willkommen im Studi-Cafe",
-                style: google_fonts.GoogleFonts.dancingScript(
-                  fontSize: 40,
+              
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.4,
+              margin: const EdgeInsets.fromLTRB(0, 50, 0, 50),
+              child: Center(
+                child: Text (
+                  "Willkommen im Studi-Cafe",
+                  style: google_fonts.GoogleFonts.dancingScript(
+                    fontSize: 40,
+                  ),
                 ),
               ),
             ),
-          ),
-          Text (
-            'unsere Sponsoren:',
-          ),    
-          SizedBox(height: 30),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(onPressed: null, child: Text('KSK')),
-              ElevatedButton(onPressed: null, child: Text('SIA')),
-            ],
-          ),
-          
-        ],
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFD98E44),
+                borderRadius: BorderRadius.circular(10.0), // Optional: Add border radius for rounded corners
+                border: Border.all(
+                  color: Colors.white, // White border color
+                  width: 8.0, // Adjust the width of the border
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch, 
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Center(
+                        child: Text(
+                          'Unsere Sponsoren/Partner:',
+                          style: google_fonts.GoogleFonts.playfairDisplay(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    ...sponsors.map((sponsor) {
+                      return Container(
+                        padding: EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(
+                              Icons.favorite,
+                              color: Colors.white, // Adjust the color as needed
+                            ),
+                            SizedBox(width: 8.0),
+                            Text(
+                              sponsor,
+                              style: google_fonts.GoogleFonts.playfairDisplay(
+                                fontSize: 16.0
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ],
+                ),
+              ),
+            ),          
+          ],
+        ),
       ),
     );
   }
