b3e6e434458e77e0eae43b84882b338156e048e6
speier-v <127931979+speier-v@users.noreply.github.com>
2024-01-14 17:28:30 +0100

    basic layout changes

--- lib/home_page/home_page_desktop.dart
+++ lib/home_page/home_page_desktop.dart
@@ -5,7 +5,7 @@ import 'package:studi_cafe/navbar_desktop.dart';
 
 class HomePageDesktop extends StatelessWidget {
   const HomePageDesktop({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,50 +22,87 @@ class HomePageDesktop extends StatelessWidget {
           // SliverToBoxAdapter for the rest of the content
           SliverToBoxAdapter(
             child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/homepage/experimental_background.jpg'), // Replace with your image asset path
+                  fit: BoxFit.cover, // Adjust the fit as needed
+                ),
+              ),
+              child: Row(
                 children: [
-                  const SizedBox(height: 30),
-                  Center(
-                    child: Text(
-                      "Willkommen im Studi-Cafe",
-                      style: google_fonts.GoogleFonts.dancingScript(
-                        fontSize: 50,
+                  // Container for left purple background
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.25,
+                  ),
+                  
+                  // Container for main content with white background
+                  Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: MediaQuery.of(context).size.height,
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 30),
+                          Container(
+                            height: 300,
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFD98E44),
+                              borderRadius: BorderRadius.circular(10.0), // Optional: Add border radius for rounded corners
+                              border: Border.all(
+                                color: Colors.white, // White border color
+                                width: 8.0, // Adjust the width of the border
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Willkommen im Studi-Cafe",
+                                style: google_fonts.GoogleFonts.dancingScript(
+                                  fontSize: 50,
+                                ),
+                              ),
+                            ),
+                          ),
+                          // Project Image
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.transparent, width: 20),
+                            ),
+                            child: Image.asset(
+                              'assets/logo/app_logo.png', // Replace with your image path
+                              width: 200,
+                              height: 200,
+                            ),
+                          ),
+
+                          // Placeholder Text
+                          const SizedBox(height: 20),
+                          const Text('Placeholder Text'),
+
+                          // Opening Hours
+                          const SizedBox(height: 20),
+                          const Text('Opening Hours: Monday-Sunday, 8:00 AM - 10:00 PM'),
+
+                          // How to Get There
+                          const SizedBox(height: 20),
+                          const Text('How to Get There: Your directions go here'),
+
+                          // Raumbuchung Button
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Add navigation logic to Raumbuchung page
+                            },
+                            child: const Text('Raumbuchung'),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  // Project Image
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.transparent, width: 20),
-                    ),
-                    child: Image.asset(
-                      'assets/logo/app_logo.png', // Replace with your image path
-                      width: 200,
-                      height: 200,
-                    ),
-                  ),
+                  
 
-                  // Placeholder Text
-                  const SizedBox(height: 20),
-                  const Text('Placeholder Text'),
-
-                  // Opening Hours
-                  const SizedBox(height: 20),
-                  const Text('Opening Hours: Monday-Sunday, 8:00 AM - 10:00 PM'),
-
-                  // How to Get There
-                  const SizedBox(height: 20),
-                  const Text('How to Get There: Your directions go here'),
-
-                  // Raumbuchung Button
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Add navigation logic to Raumbuchung page
-                    },
-                    child: const Text('Raumbuchung'),
+                  // Container for right purple background
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.25,
                   ),
                 ],
               ),
@@ -77,7 +114,6 @@ class HomePageDesktop extends StatelessWidget {
     );
   }
 }
-
 
 class _StickyNavbarDelegate extends SliverPersistentHeaderDelegate {
   final Widget child;
