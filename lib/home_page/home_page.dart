671bc23a154e50259dc8f6271cf7bcb4b2588621
speier-v <127931979+speier-v@users.noreply.github.com>
2024-01-15 19:39:51 +0100

    remove database access from homepage state

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:studi_cafe/home_page/home_page_desktop.dart';
 import 'package:studi_cafe/home_page/home_page_mobile.dart';
 import 'package:studi_cafe/responsive/responsive_layout.dart';
-import 'package:studi_cafe/home_page/datenbankverbindung.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,18 +11,6 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  late DatabaseStream _databaseStream;
-
-  @override
-  void initState() {
-    super.initState();
-    _databaseStream = DatabaseStream(
-      onDataLoaded: () {
-        setState(() {});
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
